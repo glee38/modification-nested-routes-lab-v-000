8222d67205fec24b7b682aa6e172690082f2560f
glee38 <glee3388@gmail.com>
2016-05-03 01:08:42 -0400

    add notes

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -7,8 +7,20 @@ module ArtistsHelper
     if artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
-      display_artist(song)
+      artist.name
     end
   end
+
+  # solution on learn
+  # added @nested = true flag to post#edit if params[:artist_id]
+  # seems a little roundabout - above solution does not need the flag and does the same thing because logically, if there is a @song.artist, then it is nested
+
+  # def artist_select(nested, song)
+  #   if nested
+  #     song.artist.name
+  #   else
+  #     select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+  #   end
+  # end
 
 end
